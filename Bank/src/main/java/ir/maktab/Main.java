8add1fb5ee50540e28fd5e05e6abe5019f6fd034
mahdikarimi422@gmail.com <09388990352@>
2022-03-27 18:59:36 +0430

    create project.

---
+++ Bank/src/main/java/ir/maktab/Main.java
@@ -0,0 +1,7 @@
+package ir.maktab;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Bank/src/main/java/ir/maktab/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package ir.maktab.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseEntity {
+
+    private int id;
+
+
+}
